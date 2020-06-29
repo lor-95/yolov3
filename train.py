2bc49ff93e15ba6b8f0cbdfed02fcc81787317d8
Lorenzo Giuliani <lory_extr3me@hotmail.it>
2020-06-29 10:47:11 +0200

    fix

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ except:
     print('Apex recommended for faster mixed precision training: https://github.com/NVIDIA/apex')
     mixed_precision = False  # not installed
 
-wdir = 'weights' + os.sep  # weights dir
+wdir = 'yolov3/weights' + os.sep  # weights dir
 last = wdir + 'last.pt'
 best = wdir + 'best.pt'
 results_file = 'results.txt'
@@ -34,13 +34,13 @@ hyp = {'giou': 3.54,  # giou loss gain
        'momentum': 0.937,  # SGD momentum
        'weight_decay': 0.0005,  # optimizer weight decay
        'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
-       'hsv_h': 0.0138,  # image HSV-Hue augmentation (fraction)
-       'hsv_s': 0.678,  # image HSV-Saturation augmentation (fraction)
-       'hsv_v': 0.36,  # image HSV-Value augmentation (fraction)
-       'degrees': 1.98 * 0,  # image rotation (+/- deg)
-       'translate': 0.05 * 0,  # image translation (+/- fraction)
-       'scale': 0.05 * 0,  # image scale (+/- gain)
-       'shear': 0.641 * 0}  # image shear (+/- deg)
+       'hsv_h': 0,  # image HSV-Hue augmentation (fraction)
+       'hsv_s': 0,  # image HSV-Saturation augmentation (fraction)
+       'hsv_v': 0,  # image HSV-Value augmentation (fraction)
+       'degrees': 0,  # image rotation (+/- deg)
+       'translate': 0,  # image translation (+/- fraction)
+       'scale': 0,  # image scale (+/- gain)
+       'shear': 0}  # image shear (+/- deg)
 
 # Overwrite hyp with hyp*.txt (optional)
 f = glob.glob('hyp*.txt')
